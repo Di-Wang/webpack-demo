77beb3208d7356619446b5ff29f2db8f82745bf7
Di-Wang <45746128+Di-Wang@users.noreply.github.com>
2020-05-26 17:26:13 +0800

    动态添加入口 并生成html

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,103 @@
 
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-
+const glob = require('glob');
+// 清除目录
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// html模板
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 分离css
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// 压缩css
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
+// 多页面打包
+const setMPA = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+
+  const entryFiles = glob.sync(path.join(__dirname, './src/pages/**/index.js'))
+
+  Object.keys(entryFiles).map(index => {
+    const entryFile = entryFiles[index];
+    const match = entryFile.match(/src\/pages\/(.*)\/index\.js/);
+    const pageName = match && match[1];
+
+    entry[pageName] = entryFile;
+    htmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/pages/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false
+        }
+      })
+    );
+  })
+  console.log('entryFiles', entryFiles);
+
+  return {
+    entry,
+    htmlWebpackPlugins
+  }
+}
+
+const { entry, htmlWebpackPlugins } = setMPA();
+
+
+//动态添加入口
+// function getEntry() {
+//   var entry = {};
+//   //读取src目录所有page入口
+//   glob.sync('./src/pages/**/*.js').forEach(function (name) {
+//     console.log(name);
+
+//     var start = name.indexOf('src/') + 4;
+//     var end = name.length - 3;
+//     var eArr = [];
+//     var n = name.slice(start, end);
+//     n = n.split('/')[1];
+//     eArr.push(name);
+//     eArr.push('babel-polyfill');
+//     entry[n] = eArr;
+//   })
+//   return entry;
+// }
+//动态生成html
+// //获取html-webpack-plugin参数的方法
+// var getHtmlConfig = function (name, chunks) {
+//   return {
+//     template: `./src/pages/${name}.html`,
+//     filename: `pages/${name}.html`,
+//     inject: true,
+//     hash: false,
+//     chunks: [name]
+//   }
+// }
+// //配置页面
+// var entryObj = getEntry();
+// var htmlArray = [];
+// Object.keys(entryObj).forEach(function (element) {
+//   htmlArray.push({
+//     _html: element,
+//     title: '',
+//     chunks: [element]
+//   })
+// })
+// //自动生成html模板
+// htmlArray.forEach(function (element) {
+//   module.exports.plugins.push(new HtmlWebpackPlugin(getHtmlConfig(element._html, element.chunks)));
+// })
+
 module.exports = {
-  entry: {
-    page1: './src/pages/page1/index.js',
-    page2: './src/pages/page2/index.js',
-  },
+  entry,
   output: {
     filename: './js/[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -45,7 +131,7 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }, 
+      },
       {
         test: /\.(png|jpe?g|gif)$/,
         use: [{
@@ -78,25 +164,16 @@ module.exports = {
     ]
   },
   plugins: [
+    //热更新模块，这样js改变就不会全部重载，而是只是重载你改过的那一部分
     new webpack.HotModuleReplacementPlugin(),
     // 调用之前先清除
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'page1.html',
-      template: './src/pages/page1/index.html',
-      chunks: ['page1']
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'page2.html',
-      template: './src/pages/page2/index.html',
-      chunks: ['page2']
-    }),
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
       chunkFilename: 'css/[id].css'
     }),
     new OptimizeCssAssetsPlugin()
-  ]
+  ].concat(htmlWebpackPlugins)
 }
 
 
